92938c7c85d47bac002bebe5641583f590ca7b3e
ryan10599 <thanapol.ryan@mail.kmutt.ac.th>
2021-01-31 19:43:21 +0700

    [EDIT] move main.tf & variable.tf

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "git_username" {
+    description = "username for clone repo from github"
+    default = ""
+    type = string
+    sensitive = true
+}
+
+variable "git_password" {
+  description = "password for clone repo from github"
+  default = ""
+  type = string
+  sensitive = true
+}
+
+variable "azure_username" {
+  description = "Username for remote to azure server"
+  default = ""
+  type = string
+  sensitive = true
+}
+
+variable "azure_password" {
+    description = "Password for remtoe to azure server"
+    default = "@1234"
+    type = string
+    sensitive = true
+  
+}
